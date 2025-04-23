1a7614e4871d4c39df085eab6fd465cb24ccf5a9
mohamein <ameinismail3144@gmail.com>
2025-04-23 00:26:14 -0700

    added print functionality

--- app/(root)/dashboard/receipt/page.tsx
+++ app/(root)/dashboard/receipt/page.tsx
@@ -5,7 +5,8 @@ import ReceiptTable from "@/components/tables/ReceiptTable";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { getArdiyad } from "@/lib/actions/ardiyad.action";
-
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 interface ReceiptData {
   id: number;
   place: string;
@@ -36,6 +37,24 @@ const ReceiptComponent = () => {
     fetchData();
   }, []);
 
+  const contentRef = useRef<HTMLDivElement>(null);
+  const handlePrint = useReactToPrint({
+    // content: contentRef.current,
+    contentRef: contentRef,
+    bodyClass: "print-body",
+    pageStyle: `@media print { @page { margin: 0; } body { margin: 20px; } }`,
+    documentTitle: "Receipt",
+    onAfterPrint: () => alert("Print successful!"),
+  });
+
+  const handlePrintWithCheck = () => {
+    if (contentRef.current) {
+      handlePrint();
+    } else {
+      console.error("No content to print");
+    }
+  };
+
   return (
     <div className="px-4">
       <div className="flex items-center justify-between w-full">
@@ -43,17 +62,21 @@ const ReceiptComponent = () => {
         <div className="flex items-center gap-2">
           <Link
             href="/dashboard/receipt/add"
-            className="bg-green-500 text-white px-4 py-2 rounded-md"
+            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-700 transition-all duration-500 ease-in-out"
           >
             Add Receipt
           </Link>
-          <Button className="bg-gray-200 text-gray-700 px-4 py-2 rounded-md ml-2">
+          <Button
+            onClick={handlePrintWithCheck}
+            size="lg"
+            className="bg-transparent border border-green-500 text-green-600 px-4 py-2 rounded-md ml-2 hover:bg-green-500 hover:text-white transition duration-500 ease-in-out"
+          >
             Print Receipt
           </Button>
         </div>
       </div>
 
-      <div className="w-full h-full mt-4">
+      <div className="w-full h-full mt-4" ref={contentRef}>
         {/* <!-- Receipt Table Component Here --!> */}
         <ReceiptTable data={data} />
       </div>
